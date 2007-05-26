5db13dc413ab0c416947b86d13c0647a1451fd5b
Clive Crous <clive@crous.co.za>
2007-05-26 11:02:47 +0000

    r49@clive-laptop (orig r214):  clive | 2007-05-24 11:58:12 +0200  Add css helpers for floats too

--- lib/remarkably/engines/css/helpers.rb
+++ lib/remarkably/engines/css/helpers.rb
@@ -1,5 +1,7 @@
-class Fixnum
-  %w{em ex cm in mm pt pc px}.each do |unit|
-    define_method( unit ) {"#{self}#{unit}"}
+[ Fixnum, Float ].each do |extend_class|
+  extend_class.class_eval do
+    %w{em ex cm in mm pt pc px}.each do |unit|
+      define_method( unit ) {"#{self}#{unit}"}
+    end
   end
 end
