d72c4bba282bc2e9a6a301b12469011842d706d0
Clive Crous <clive@crous.co.za>
2007-05-26 14:09:21 +0000

    CSS classes, ids and pseudo classes

--- lib/remarkably/engines/css.rb
+++ lib/remarkably/engines/css.rb
@@ -5,10 +5,12 @@ module Remarkably
     class CSS < Base::Engine
       def method! sym, args, hash, &block
         sym = sym.to_s.downcase
+        sym = "##{sym}".chop if sym[-1].chr == '!'
+        sym = ".#{sym}".chop if sym[-1].chr == '?'
 
         if block_given?
           current_prefix = @css_prefix
-          @css_prefix = (@css_prefix+" #{sym}#{args.map{|a|a.to_s}.join}").strip
+          @css_prefix = (@css_prefix+"#{sym}#{args.map{|a|a.to_s}.join} ")
           @css_depth+=1
           @css_prefix_rendered=false
           block.call
@@ -20,13 +22,32 @@ module Remarkably
               @output.chop!
               @output << "}"
             end
-            @output << "\n#{@css_prefix} {"
+            @output << "\n#{@css_prefix}{"
             @css_prefix_rendered = true
             @css_first_use = false
           end
           @output << "#{sym.gsub('_','-')}:#{args.map{|a|a.to_s}.join(' ')};"
         end
         self
+      end
+
+      def css_prefix prefix, &block
+        @css_prefix << prefix
+        block.call
+        @css_prefix.chop!
+        self
+      end
+
+      def css_class args, hash, &block
+        css_prefix ".", &block
+      end
+
+      def css_id args, hash, &block
+        css_prefix "#", &block
+      end
+
+      def css_pseudo args, hash, &block
+        css_prefix ":", &block
       end
 
       def to_s
--- spec/engines/css.rb
+++ spec/engines/css.rb
@@ -19,4 +19,44 @@ describe Remarkably::Engines::CSS do
     end.to_s.should == "body p {background-color:white;border:1px solid black}\nbody div {color:blue}\n"
   end
 
+  it "uses css class" do
+    div do
+      css_class do
+        foo do
+          color :red
+        end
+      end
+      bar? do
+        color :blue
+      end
+      P do
+        color :green
+      end
+    end.to_s.should == "div .foo {color:red}\ndiv .bar {color:blue}\ndiv p {color:green}\n"
+  end
+
+  it "uses css id" do
+    div do
+      css_id do
+        foo do
+          color :red
+        end
+        bar do
+          color :blue
+        end
+      end
+      baz! do
+        color :green
+      end
+    end.to_s.should == "div #foo {color:red}\ndiv #bar {color:blue}\ndiv #baz {color:green}\n"
+  end
+
+  it "uses css pseudo" do
+    css_pseudo do
+      hover do
+        color :red
+      end
+    end.to_s.should == ":hover {color:red}\n"
+  end
+
 end
